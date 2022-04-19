eddff20fcfb4cdbe6d88b88219674a625c52eaf2
bambang.ap <bambang.ap@flash-coffee.com>
2022-04-20 05:50:19 +0700

    alias helper i

--- src/installApp.js
+++ src/installApp.js
@@ -26,31 +26,38 @@ const commander_1 = require("commander");
 const methods_1 = require("../methods");
 const outputFolder = "outputs";
 async function installApp() {
-    const deviceLists = (0, child_process_1.execSync)('adb devices').toString().split('\n')
+    const deviceLists = (0, child_process_1.execSync)("adb devices")
+        .toString()
+        .split("\n")
         .reduce((ret, list) => {
-        const index = list.indexOf('\t');
+        const index = list.indexOf("\t");
         if (index > 0)
             ret.push(list.slice(0, index));
         return ret;
     }, []);
     const fileLists = (0, child_process_1.execSync)(`ls ${outputFolder}`).toString();
-    const { selectedDevice } = await inquirer.prompt([{
+    const { selectedDevice } = await inquirer.prompt([
+        {
             type: "list",
             name: "selectedDevice",
             message: "Select device you want to set as target install",
-            choices: deviceLists
-        }]);
-    const choices = fileLists.split('\n').filter(l => l !== '');
-    const { selectedApk } = await inquirer.prompt([{
+            choices: deviceLists,
+        },
+    ]);
+    const choices = fileLists.split("\n").filter((l) => l !== "");
+    const { selectedApk } = await inquirer.prompt([
+        {
             type: "list",
             name: "selectedApk",
             message: "Select apk you want to install",
-            choices
-        }]);
+            choices,
+        },
+    ]);
     (0, methods_1.thread)(`adb -s ${selectedDevice} install "${methods_1.ROOT_PATH}/${outputFolder}/${selectedApk}"`);
 }
 const installAppCommand = () => commander_1.program
-    .command('install')
-    .description('Install apk file from list in /outputs folder')
+    .command("install")
+    .alias("i")
+    .description("Install apk file from list in /outputs folder")
     .action(installApp);
 exports.installAppCommand = installAppCommand;
